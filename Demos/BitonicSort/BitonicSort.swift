aa4bfb79ec231768599ae573ef21b7b36b62d813
Ben Medina <bmedina@fastmail.com>
2026-02-05 11:51:10 -0800

    Add BitonicSort executable target.

---
+++ Demos/BitonicSort/BitonicSort.swift
@@ -0,0 +1,15 @@
+// Copyright 2026 Adobe
+// All Rights Reserved.
+//
+// NOTICE: Adobe permits you to use, modify, and distribute this file in
+// accordance with the terms of the Adobe license agreement accompanying
+// it.
+//
+
+@main
+struct Main {
+    static func main() throws {
+        print ("BitonicSort not yet implemented.")
+        // try runDemo(title: "Bitonic Sort", provider: BitonicSortDemo())
+    }
+}
