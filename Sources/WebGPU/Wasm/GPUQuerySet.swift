4d592d875f9e32da4b785a3a6e03e9e1b4464859
Ben Medina <bmedina@fastmail.com>
2026-03-12 14:48:54 -0700

    Add GPUQuerySet.

---
+++ Sources/WebGPU/Wasm/GPUQuerySet.swift
@@ -0,0 +1,39 @@
+// Copyright 2026 Adobe
+// All Rights Reserved.
+//
+// NOTICE: Adobe permits you to use, modify, and distribute this file in
+// accordance with the terms of the Adobe license agreement accompanying
+// it.
+
+import JavaScriptKit
+
+@JSClass public struct GPUQuerySet {
+	// @JSSetter macro requires `set` prefix
+	@JSSetter(jsName: "label") func setLabel_(_ value: String?) throws(JSException)
+
+	public func setLabel(_ value: String?) {
+		try! setLabel_(value)
+	}
+
+	@JSGetter(jsName: "type") var _type: String
+
+	public var type: GPUQueryType {
+		get {
+			let s = try! _type
+			return GPUQueryType(rawValue: s) ?? .occlusion
+		}
+	}
+
+	@JSGetter(jsName: "count") var _count: Int
+
+	public var count: Int {
+		get { return try! _count }
+	}
+
+	@JSFunction(jsName: "destroy")
+	func _destroy() throws(JSException)
+
+	public func destroy() {
+		try! _destroy()
+	}
+}
